508a4fb3f27d02aedd63510a837494d8dbdc28c2
Shashwat Shah <69806791+shashwat-shah@users.noreply.github.com>
2022-06-30 11:48:14 +0530

    Initial Commit

---
+++ Design4.py
@@ -0,0 +1,14 @@
+#Shashwat Shah
+
+import turtle
+
+t=turtle.Turtle()
+t.speed(100)
+turtle.bgcolor("black")
+
+for item in range(240):
+ t.color("aqua")
+ t.circle(item)
+ t.left(5)
+
+turtle.done()
